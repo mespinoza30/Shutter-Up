a8638b38009ea81795d77d143933dc77afa9e7b6
mespinoza30 <m.espinoza16@hotmail.com>
2021-02-13 01:53:09 -0800

    Added Cloudinary

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const layouts = require('express-ejs-layouts');
 const app = express();
 const multer = require('multer'); //used for uploading files
 const cloudinary = require('cloudinary');
+//uploader for images, make a uploads forlder, then pass through the route as middleware
+const uploads = multer({ dest: './uploads'});
 
 app.set('view engine', 'ejs');
 
@@ -22,7 +24,28 @@ app.get('/profile', (req, res) => {
 
 app.use('/auth', require('./routes/auth'));
 
+app.get('/images', (req, res) => {
+  res.render('index');
+})
 
+app.get('/images/new', (req, res) => {
+  res.render('new');
+})
+
+app.post('/images', uploads.single('inputFile'), (req, res) => {
+  //grab the uploaded file
+  const image = req.file.path;
+  console.log(image);
+  //upload image to cloudinary
+  cloudinary.uploader.upload(image, (result) => {
+    //the result that comes back from cloudinary
+    console.log(result);
+    //its getting this info from the info that it returns in the terminal
+    res.render('index', { image: result.url })
+  })
+})
+
+//Listen on PORT
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
